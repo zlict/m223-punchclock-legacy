11cee874b2508c49f66c533688e9fba0b26bc276
Robin <1105080+openscript@users.noreply.github.com>
2019-10-31 14:08:57 +0100

    Add example client

---
+++ src/main/resources/public/script.js
@@ -0,0 +1,63 @@
+const URL = 'http://localhost:8081';
+let entries = [];
+
+const dateAndTimeToDate = (dateString, timeString) => {
+    return new Date(`${dateString}T${timeString}`).toISOString();
+};
+
+const createEntry = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const entry = {};
+    entry['checkIn'] = dateAndTimeToDate(formData.get('checkInDate'), formData.get('checkInTime'));
+    entry['checkOut'] = dateAndTimeToDate(formData.get('checkOutDate'), formData.get('checkOutTime'));
+
+    fetch(`${URL}/entries`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(entry)
+    }).then((result) => {
+        result.json().then((entry) => {
+            entries.push(entry);
+            renderEntries();
+        });
+    });
+};
+
+const indexEntries = () => {
+    fetch(`${URL}/entries`, {
+        method: 'GET'
+    }).then((result) => {
+        result.json().then((result) => {
+            entries = result;
+            renderEntries();
+        });
+    });
+    renderEntries();
+};
+
+const createCell = (text) => {
+    const cell = document.createElement('td');
+    cell.innerText = text;
+    return cell;
+};
+
+const renderEntries = () => {
+    const display = document.querySelector('#entryDisplay');
+    display.innerHTML = '';
+    entries.forEach((entry) => {
+        const row = document.createElement('tr');
+        row.appendChild(createCell(entry.id));
+        row.appendChild(createCell(new Date(entry.checkIn).toLocaleString()));
+        row.appendChild(createCell(new Date(entry.checkOut).toLocaleString()));
+        display.appendChild(row);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+    const createEntryForm = document.querySelector('#createEntryForm');
+    createEntryForm.addEventListener('submit', createEntry);
+    indexEntries();
+});
