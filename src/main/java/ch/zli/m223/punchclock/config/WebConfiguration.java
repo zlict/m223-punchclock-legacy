63a74d5813b65f364c107ef6d668755569aa0bb4
Robin <r+github@obin.ch>
2020-09-03 14:50:24 +0200

    Enhance web configuration

--- src/main/java/ch/zli/m223/punchclock/config/WebConfiguration.java
+++ src/main/java/ch/zli/m223/punchclock/config/WebConfiguration.java
@@ -1,26 +1,22 @@
 package ch.zli.m223.punchclock.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
+/**
+ * Also see https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-cors
+ */
 @Configuration
-@Profile("dev")
-public class WebConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .exposedHeaders(
-                        "Access-Control-Allow-Headers",
-                        "Access-Control-Allow-Origin",
-                        "Access-Control-Expose-Headers",
-                        "Authorization",
-                        "Cache-Control",
-                        "Content-Type",
-                        "Origin"
-                );
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+            }
+        }
     }
 }
